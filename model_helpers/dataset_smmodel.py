d62c9caaa7976c4f751c94ea55891a8bd5d54c29
ZLBioMed <106302199+zlbme@users.noreply.github.com>
2022-07-06 01:49:17 +0100

    Update dataset_smmodel.py

--- model_helpers/dataset_smmodel.py
+++ model_helpers/dataset_smmodel.py
@@ -1,5 +1,7 @@
-# Author: Zhe Liu
-# Date: 2020
+'''
+Dataset class for single-modal EIT image reconstruction
+@author: LIU Zhe
+'''
 from torch.utils.data import Dataset, DataLoader
 import numpy as np
 from matplotlib import pyplot as plt
@@ -7,10 +9,9 @@ from matplotlib import pyplot as plt
 
 class DatasetSingleVanilla(Dataset):
     """
-    Vanilla version of single modal model data set
+    Vanilla version of dataset for single-modal imaging 
     """
     def __init__(self, v_path, img_path):
-
         super().__init__()
 
         v_np = np.load(v_path).astype(np.float32)
@@ -30,42 +31,5 @@ class DatasetSingleVanilla(Dataset):
         return v, img
 
 
-if __name__ == '__main__':
-
-    proj_path = "D:/PhD/My Projects/EIT/EIT Image Reconstruction/dmEIT2D - MSFCFNet/"
-    v_path = proj_path + 'dataSimu/obj1234_TrainSet/v_train.npy'
-    img_path = proj_path + 'dataSimu/obj1234_TrainSet/img_train.npy'
-
-    data_set = DatasetSingleVanilla(v_path, img_path)
-    train_loader = DataLoader(data_set, batch_size=50)
-
-    for batch_v, batch_img in train_loader:
-
-        print('--------------------------------------------')
-        print(batch_v.shape)
-        print(batch_img.shape)
-        print('--------------------------------------------')
-
-        slice_num = 3
-        img = batch_img.numpy()[slice_num, 0, ...]
-
-        print('--------------------------------------------')
-        print(img.shape)
-        print('--------------------------------------------')
-
-        # Display
-        plt.imshow(img, vmin=0, vmax=1, cmap='Purples', interpolation='nearest')
-        plt.title('EIT Image')
-        plt.colorbar()
-        plt.show()
-
-        break
-
-
-
-
-
-
-
-
-
+    
+    
